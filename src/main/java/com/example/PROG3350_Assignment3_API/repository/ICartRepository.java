c9b6542e8cfa51ce265595c27b20cb2dfbfd6d6b
TeaLizard <owentwilliams7@gmail.com>
2025-11-04 17:17:55 -0500

    repo annotations

--- src/main/java/com/example/PROG3350_Assignment3_API/repository/ICartRepository.java
+++ src/main/java/com/example/PROG3350_Assignment3_API/repository/ICartRepository.java
@@ -2,6 +2,8 @@ package com.example.PROG3350_Assignment3_API.repository;
 
 import com.example.PROG3350_Assignment3_API.model.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ICartRepository extends JpaRepository<Cart,Integer> {
 }
--- src/main/java/com/example/PROG3350_Assignment3_API/repository/IShoeRepository.java
+++ src/main/java/com/example/PROG3350_Assignment3_API/repository/IShoeRepository.java
@@ -2,6 +2,8 @@ package com.example.PROG3350_Assignment3_API.repository;
 
 import com.example.PROG3350_Assignment3_API.model.entity.Shoe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IShoeRepository extends JpaRepository<Shoe, Integer> {
 }
