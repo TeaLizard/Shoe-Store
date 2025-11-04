842dac8df3714f7ab6d1ca0e41f10aac9faacdc2
TeaLizard <owentwilliams7@gmail.com>
2025-11-04 13:36:43 -0500

    first commit

---
+++ src/main/java/com/example/PROG3350_Assignment3_API/Prog3350Assignment3ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.PROG3350_Assignment3_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3350Assignment3ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prog3350Assignment3ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PROG3350_Assignment3_API/Prog3350Assignment3ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PROG3350_Assignment3_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3350Assignment3ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
