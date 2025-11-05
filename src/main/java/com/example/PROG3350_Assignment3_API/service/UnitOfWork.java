b048d194d59eed47859e40b314e7fba9e4ffa839
TeaLizard <owentwilliams7@gmail.com>
2025-11-05 14:17:10 -0500

    Unit of Work update

--- src/main/java/com/example/PROG3350_Assignment3_API/service/UnitOfWork.java
+++ src/main/java/com/example/PROG3350_Assignment3_API/service/UnitOfWork.java
@@ -1,14 +1,17 @@
 package com.example.PROG3350_Assignment3_API.service;
 
+import com.example.PROG3350_Assignment3_API.model.mapper.Mapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UnitOfWork {
     private final IShoeService shoeService;
     private final ICartService cartService;
-    public UnitOfWork(IShoeService shoeService, ICartService cartService) {
+    private final Mapper mapper;
+    public UnitOfWork(IShoeService shoeService, ICartService cartService, Mapper mapper) {
         this.shoeService = shoeService;
         this.cartService = cartService;
+        this.mapper = mapper;
     }
 
     public IShoeService getShoeService() {
@@ -16,5 +19,8 @@ public class UnitOfWork {
     }
     public ICartService getCartService() {
         return cartService;
+    }
+    public Mapper getMapper() {
+        return mapper;
     }
 }
